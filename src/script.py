9de6d384828a7f6254369ab380305e7a05c624eb
theGreatHeisenberg <ps.alchemist@gmail.com>
2014-04-02 02:10:50 +0530

    Test commit

--- src/script.py
+++ src/script.py
@@ -2,6 +2,5 @@ import os,sys
 import subprocess
 import get_download
 x=get_download.get_download()
-print sys.argv[1]
 link,size=x.donwload_by_name(sys.argv[1].split("/")[-1])
 subprocess.call(["vlc",str(link)])
