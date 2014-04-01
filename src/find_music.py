b6c50a9263f7aa2939c8c5d694c279d280766b2e
Rushikesh Patil <rushikesh.patil003@gmail.com>
2014-04-02 00:40:33 +0530

    added session

--- src/find_music.py
+++ src/find_music.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup as Soup
 from re import findall
 from re import split as Split
 import logging
+import requests
 class find_music():
     names = []
     site_url = "http://www.tunefind.com"
+
     original_name = ""
     use_name = ""
+    episodes_dict = {}
     def __init__(self,name,type):
         self.url = "http://www.tunefind.com/browse/"
+        self.s = requests.session()
+        self.p = self.s.get(self.site_url)
         self.tname = name.replace(" ","-")
         self.name=name
         self.type = type
@@ -23,7 +28,7 @@ class find_music():
             url = self.url + self.type
         else:
             return []
-        req = request('GET',url)
+        req = self.s.get(url)
         soap = Soup(req.text)
         self.names = []
         for body in soap.findAll('div',{"class":"col-md-4"}):
@@ -39,32 +44,34 @@ class find_music():
 
         url = "http://www.tunefind.com/show/" + self.use_name
 #        url = "http://www.tunefind.com/show/suits"
-        req = request('GET',url)
+        req = self.s.get(url)
         seasons=[]
         soap = Soup(req.text)
 #        print req.text
         for body in soap.find_all('div',{"class":"panel panel-default tf-panel"}):
             for names in  body.find_all('a'):
                 seasons.append(names.string.encode('ascii','ignore'))
+
         return seasons
 
     def __get_episodes_dict(self,season_name):
         season_name = season_name.replace(' ','-').lower()
         url = "http://www.tunefind.com/show/"+self.get_OriginalName().lower().replace(" ","-")+"/" + season_name
-        req = request('GET',url)
+        req = self.s.get(url)
         episodes = {}
 
         soap = Soup(req.text)
         for body in soap.find_all('ul',{'class':"list-group"}):
             for episode_body in body.find_all('li',{'class':'list-group-item'}):
                 episodes[episode_body.find('a').string.encode('ascii','ignore').replace('\n','').strip(' ')] = episode_body.find('a').get('href')
+        self.episodes_dict[season_name] = episodes
         return episodes
 
     def get_episodes(self,season_name):
         return self.__get_episodes_dict(season_name).keys()
 
-    def getMusicdict(self,season_name,episode_name):
-        url = self.site_url + self.__get_episodes_dict(season_name)[episode_name]
+    def getMusicdict(self,episode_name):
+        url = self.site_url + self.episodes_dict[episode_name]
         req = request('GET',url)
         soap = Soup(req.text)
         episodes = []
@@ -91,6 +98,6 @@ logging.basicConfig(filename="log1.txt",level=logging.DEBUG,filemode="w")
 t = find_music(name='breakingBAD',type='tv')
 print t.get_OriginalName()
 print t.get_seasons()
-p1 =  t.get_episodes('Season 2')
+p1 =  t.get_episodes('Season 3')
 print p1
-print t.getMusicdict(season_name='Season 2',episode_name='9. 4 Days Out')
+print t.getMusicdict(episode_name='7. One Minute')
